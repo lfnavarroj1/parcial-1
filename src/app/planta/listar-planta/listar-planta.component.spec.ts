6cdd6fb2c65e7e5a1d93f20f7180466a5a11b865
Luis Felipe Navarro Julio <lf.navarroj1@uniandes.edu.co>
2022-11-06 07:12:06 -0500

    Pruebas de creación de filas de la tabla (incluida encabezdos) - ok

--- src/app/planta/listar-planta/listar-planta.component.spec.ts
+++ src/app/planta/listar-planta/listar-planta.component.spec.ts
@@ -2,16 +2,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
+import { faker } from "@faker-js/faker";
 
 import { ListarPlantaComponent } from './listar-planta.component';
+import { HttpClientModule } from "@angular/common/http";
+import { PlantaService } from '../planta.service';
+import { Planta } from '../planta';
 
 describe('ListarPlantaComponent', () => {
   let component: ListarPlantaComponent;
   let fixture: ComponentFixture<ListarPlantaComponent>;
+  let debug:DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ListarPlantaComponent ]
+      imports:[HttpClientModule],
+      declarations: [ ListarPlantaComponent ],
+      providers:[PlantaService]
     })
     .compileComponents();
   }));
@@ -19,10 +26,31 @@ describe('ListarPlantaComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ListarPlantaComponent);
     component = fixture.componentInstance;
+
+    for (let i = 0; i < 3; i++) {
+      const planta = new Planta(
+        faker.datatype.number(),
+        faker.datatype.string(),
+        faker.datatype.string(),
+        faker.datatype.string(),
+        faker.datatype.number(),
+        faker.datatype.string(),
+        faker.datatype.string(),
+      );
+        component.plantas.push(planta);
+    }
+
     fixture.detectChanges();
+    debug=fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('Validando se se crean las 3 filas + encabezados',()=>{
+    expect(debug.queryAll(By.css('tr'))).toHaveSize(4)
+  });
+
+
 });
